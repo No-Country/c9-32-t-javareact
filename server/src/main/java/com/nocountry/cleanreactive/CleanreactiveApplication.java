23b6ba51df3b283a8b37f65a58045f524332ae2d
kiritojobs <kiritojobs@gmail.com>
2023-02-03 17:20:51 -0500

    boilerplate springboot

---
+++ server/src/main/java/com/nocountry/cleanreactive/CleanreactiveApplication.java
@@ -0,0 +1,13 @@
+package com.nocountry.cleanreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanreactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanreactiveApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/nocountry/cleanreactive/CleanreactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nocountry.cleanreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanreactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
