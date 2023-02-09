f05beccb1a0efb293c6599fb73af36bc993bbefc
JulianRiedinger7 <julianriedinger27@gmail.com>
2023-02-09 14:24:00 -0300

    feat: created footer component
    created footer component & util classes using tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
+import { Footer } from './components/layout';
 
 function App() {
 	const [count, setCount] = useState(0);
 
-	return <div className="App">main</div>;
+	return (
+		<div className="App">
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
