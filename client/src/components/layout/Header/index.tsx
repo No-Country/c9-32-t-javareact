aaa027edcad4ffbe1b0a97266e6b28b75bad8b84
Fausto Savoya <savoyafausto@gmail.com>
2023-02-09 18:14:26 -0300

    [feature] changed nav and fotter bg-color and improved hamburguer menu animation

--- client/src/components/layout/Header/index.tsx
+++ client/src/components/layout/Header/index.tsx
@@ -10,31 +10,35 @@ const Nav = () => {
 	const navItems = ['lorem', 'lorem', 'lorem', 'lorem'];
 
 	const isSmall = useMediaQuery('(min-width: 1024px)');
+
+
 	useEffect(() => {
 		showNav
 			? (document.body.style.overflow = 'hidden')
 			: (document.body.style.overflow = 'scroll');
 	}, [showNav]);
 
+
+	
 	const variants = !isSmall && {
 		open: { opeacity: 0, x: 0 },
 		closed: { opacity: 0, x: '100%' },
 	};
 	const variantList = !isSmall && {
 		open: { opeacity: 0, y: 0 },
-		closed: { opacity: -100, y: 0 },
+		closed: { opacity: 100, y: 0 },
 		hovering: {
 			scale: 1.2,
 			transition: { delay: 0.1, duration: 0.2 },
 		},
 	};
 	return (
-		<div className="relative w-limited  ">
+		<div className="relative w-limited   ">
 			<motion.nav
 				animate={showNav ? 'open' : 'closed'}
 				variants={variants || undefined}
 				transition={{ duration: 0.5 }}
-				className="flex  bg-bgColor flex-col justify-center items-center lg:flex-row "
+				className="flex flex-col justify-center items-center lg:flex-row "
 			>
 				<motion.div
 					className="nav-list_container "
