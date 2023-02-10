71aa8949e350b05ba180e26739cf641993a6543f
Fausto Savoya <savoyafausto@gmail.com>
2023-02-10 20:59:44 -0300

    [FEATURE] Fixed footer responsive & added types for users, services and employees

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -2,4 +2,30 @@ export interface IGlobalContext {
 	children: JSX.Element | JSX.Element[];
 }
 
+export interface IUser {
+	id: string
+	name: string
+	email: string
+	password: string
+	address: string
+	phoneNumber: number
+}
 
+export interface ICleanService {
+	id: string
+	serviceType: string
+	freeTime:Date
+	address:string
+	price:number
+	state:string
+}
+
+
+export interface IEmployee{
+	id:string
+	name:string
+	email: string
+	password: string
+	address: string
+	isaVailable:string
+}
