961a80ebb19e9ac1e51e618c35c9932225a040fb
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2023-02-13 13:53:51 -0600

    [UPDATE] Hero section
    -design change to match the figma mockup

--- client/src/pages/Home/Hero.tsx
+++ client/src/pages/Home/Hero.tsx
@@ -5,25 +5,24 @@ function Hero(): ReactElement {
 		<section className="text-gray-600 body-font">
 			<div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
 				<div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-					<h1 className=" sm:text-6xl text-3xl mb-4  text-gray-900 ">
-						Profesional 🧹💧 Cleaning service
-						<span className=" text-blue-600 font-semibold">
-							{' '}
-							for your home
-						</span>
+					<h1 className=" sm:text-7xl text-3xl mb-4  text-gray-900 ">
+						Olvídate de limpiar por un momento🧹💧
 					</h1>
-					<p className="mb-8 leading-relaxed">
-						Deja la limpieza a los Expertos. Mantén tu hogar y
-						espacios limpios y libre de gérmenes, virus y hongos con
-						nuestros servicios de limpieza
+					<p className="mb-8 leading-relaxed text-2xl">
+						Con
+						<span className=" text-customViolet font-semibold">
+							{' '}
+							DustBusters
+						</span>
+						, contrata a un limpiador por el tiempo que necesites
 					</p>
 					<div className="flex justify-center">
-						<button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-							Registrarse
+						<button className="inline-flex text-white bg-royalBlue border-0 py-2 px-6 focus:outline-none hover:bg-royalBlue-600 rounded text-lg">
+							Buscar Servicio
 						</button>
-						<button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
+						{/* <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">
 							Iniciar sesión
-						</button>
+						</button> */}
 					</div>
 				</div>
 				<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
--- client/tailwind.config.cjs
+++ client/tailwind.config.cjs
@@ -1,17 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        customViolet: '#2463EB'
-      },
-      fontFamily:{
-          roboto: "'Roboto', sans-serif"
-      }
-    },
-  },
-  plugins: [],
-}
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			colors: {
+				customViolet: '#2463EB',
+				royalBlue: {
+					DEFAULT: '#2463EB',
+					50: '#CCDBFA',
+					100: '#BACEF9',
+					200: '#94B3F5',
+					300: '#6F98F2',
+					400: '#497EEE',
+					500: '#2463EB',
+					600: '#124BC5',
+					700: '#0D3792',
+					800: '#09245E',
+					900: '#04102B',
+				},
+			},
+			fontFamily: {
+				roboto: "'Roboto', sans-serif",
+			},
+		},
+	},
+	plugins: [],
+};
