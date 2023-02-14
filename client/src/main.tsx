3f54cdadba40126ab53a8d287517d94acd21c3f1
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2023-02-13 19:52:24 -0600

    [FEAT] Reviews Section for landing page
    -add ReviewCard component
    -add google material icons cdn link

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,5 @@ import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<StrictMode>
-		<App />
-	</StrictMode>,
+	<App />,
 );
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
 import { ReactElement } from 'react';
 import Hero from './Hero';
 
 import Services from './Services';
-
-
+import Reviews from './reviews';
 
 function HomePage(): ReactElement {
 	return (
 		<>
 			<Hero />
 			<Services />
+			<Reviews />
 		</>
 	);
 }
