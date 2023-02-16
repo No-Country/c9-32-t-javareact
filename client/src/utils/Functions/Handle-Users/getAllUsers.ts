64b37ac0c2d51b84419e2010ca1d817ce77effce
Fausto Savoya <savoyafausto@gmail.com>
2023-02-16 14:33:22 -0300

    [ADD] function to register user

--- client/src/utils/Functions/Handle-Users/getAllUsers.ts
+++ client/src/utils/Functions/Handle-Users/getAllUsers.ts
@@ -1,8 +1,8 @@
 const getAllUsers = async () => {
 
   const response = await fetch('https://c9-32-t-javareact-production.up.railway.app/api/v1/users', {
-    'mode': 'no-cors',
-    'headers': {
+    mode: 'no-cors',
+    headers: {
       'Access-Control-Allow-Origin': '*',
     }
   })
