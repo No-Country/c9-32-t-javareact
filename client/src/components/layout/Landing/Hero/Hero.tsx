e106a9e7512093b2530a8ff637d60613d46232f0
JulianRiedinger7 <julianriedinger27@gmail.com>
2023-02-16 17:16:16 -0300

    [feature] - updated link to login & signup in mobile

--- client/src/components/layout/Landing/Hero/Hero.tsx
+++ client/src/components/layout/Landing/Hero/Hero.tsx
@@ -1,10 +1,8 @@
 import hero_img from '@assets/hero_img.png';
 import { ReactElement } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Hero(): ReactElement {
-	const navigate = useNavigate();
-
 	return (
 		<section className="text-gray-600 body-font">
 			<div className="container mx-auto flex py-8 md:py-24 md:flex-row flex-col items-center justify-between">
@@ -24,12 +22,18 @@ function Hero(): ReactElement {
 						<button className="hidden md:block w-full text-white bg-royalBlue border-0 py-2 px-6 focus:outline-none hover:bg-royalBlue-600 rounded-[100px] text-lg text-center">
 							Buscar Servicio
 						</button>
-						<button className="md:hidden w-full text-white bg-royalBlue border-0 py-2 px-6 focus:outline-none hover:bg-royalBlue-600 rounded-[100px] text-lg text-center" onClick={() => navigate('/login')}>
+						<Link
+							to="/login"
+							className="md:hidden w-full text-white bg-royalBlue border-0 py-2 px-6 focus:outline-none hover:bg-royalBlue-600 rounded-[100px] text-lg text-center"
+						>
 							Iniciar sesión
-						</button>
-						<button className="md:hidden w-full text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded-[100px] text-lg text-center">
+						</Link>
+						<Link
+							to="/register"
+							className="md:hidden w-full text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded-[100px] text-lg text-center"
+						>
 							Registrarse
-						</button>
+						</Link>
 					</div>
 				</div>
 				<div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
