09b63b0a2abbc2f9506d1336f7b8be09681bde5e
Fausto Savoya <savoyafausto@gmail.com>
2023-02-16 10:36:20 -0300

    [ADD] function to get all users

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import Router from './utils/Router';
 import GlobalContext from './context/GlobalContext';
+import getAllUsers from './utils/functions/getUsers';
 
 function App() {
+	getAllUsers()
 	return (
 		<GlobalContext.Provider value={{}}>
 			<Router />
