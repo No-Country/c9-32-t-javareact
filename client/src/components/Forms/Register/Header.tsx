a2e7d54eeafea615d1f06e61b13ec67d596b55c2
Juan Antonio Ayola Cortes <antonio.ayola.cortes@gmail.com>
2023-02-17 17:03:43 -0600

    [ADD] login & signup header

---
+++ client/src/components/Forms/Register/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '@assets/logo-white.png';
+
+function Header() {
+	return (
+		<header>
+			<nav className="py-4 bg-royalBlue-500">
+				<Link
+					to="/"
+					title="home"
+					className="flex justify-center items-center gap-4 text-white"
+				>
+					<h1 className="text-3xl sm:text-4xl font-bold">
+						DustBusters
+					</h1>
+					<img src={logo} className="w-8" />
+				</Link>
+			</nav>
+		</header>
+	);
+}
+
+export default Header;
--- client/src/components/Forms/Register/index.tsx
+++ client/src/components/Forms/Register/index.tsx
@@ -1,14 +1,18 @@
+import Header from './Header';
 import RegisterForm from './RegisterForm';
 import Terms from './Terms';
 import Welcome from './Welcome';
 
 const Register = () => {
 	return (
-		<main className="m-auto container">
-			<Welcome />
-			<RegisterForm />
-			<Terms />
-		</main>
+		<>
+			<Header />
+			<main className="m-auto container">
+				<Welcome />
+				<RegisterForm />
+				<Terms />
+			</main>
+		</>
 	);
 };
 export default Register;
