b682e2b07479bcfbd9b51785f3f43cd8bb8284b7
Fausto Savoya <savoyafausto@gmail.com>
2023-02-24 22:54:21 -0300

    changed logo font

--- client/src/components/Forms/Register/Header.tsx
+++ client/src/components/Forms/Register/Header.tsx
@@ -5,16 +5,16 @@ import logo from '@assets/logo-white.png';
 function Header() {
 	return (
 		<header>
-			<nav className="py-4 bg-royalBlue-500 flex items-center justify-center">
+			<nav className="py-4 bg-royalBlue-500 flex items-center justify-center md:bg-transparent ">
 				<Link
 					to="/"
 					title="home"
-					className="flex justify-center items-center gap-4 text-white"
+					className="flex justify-center items-center gap-4 text-customViolet"
 				>
-					<h1 className="text-xl sm:text-xl font-extrabold">
+					<h1 className="text-xl sm:text-xl font-extrabold md:text-2xl xl:text-3xl">
 						DustBusters
 					</h1>
-					<img src={logo} className="w-8" />
+					<img src={logo} className="w-8 md:hidden" />
 				</Link>
 			</nav>
 		</header>
