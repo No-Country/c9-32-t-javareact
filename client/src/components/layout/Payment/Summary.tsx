a52974b45e31438dedd3d65a7ba47c8fe6d184d5
Fausto Savoya <savoyafausto@gmail.com>
2023-02-28 20:37:54 -0300

    [FEATURE] added styles for payment texts

--- client/src/components/layout/Payment/Summary.tsx
+++ client/src/components/layout/Payment/Summary.tsx
@@ -3,27 +3,27 @@ const Summary = () => {
     <div className="w-full divide-y-[1px]">
       <h2 className="heading3 text-center">Resumen de compra</h2>
       <div className="flex flex-col  w-full  p-3">
-        <span className="opacity-60">Servicio</span>
+        <span className="payment-label">Servicio</span>
         <span>Limpieza de hogar</span>
       </div>
       <div className="flex flex-col  w-full  p-3">
-        <span className="opacity-60">Tipo de limpieza</span>
+        <span className="opacity-60 text-customViolet">Tipo de limpieza</span>
         <span>Simple</span>
       </div>
       <div className="flex flex-col  w-full  p-3">
-        <span className="opacity-60">Tamaño de tu hogar</span>
+        <span className="opacity-60 text-customViolet">Tamaño de tu hogar</span>
         <span>Mediano (100m²)</span>
       </div>
       <div className="flex flex-col  w-full  p-3">
-        <span className="opacity-60">Turno para la limpieza</span>
+        <span className="opacity-60 text-customViolet">Turno para la limpieza</span>
         <span>Mañana (de 8 a 12hs)</span>
       </div>
       <div className="flex flex-col  w-full  p-3">
-        <span className="opacity-60">Frecuencia de limpieza</span>
+        <span className="opacity-60 text-customViolet">Frecuencia de limpieza</span>
         <span>Todos los días</span>
       </div>
       <div className="flex flex-col  w-full  p-3">
-        <span className="opacity-60">Total a pagar - Servicio mensual</span>
+        <span className="opacity-60 text-customViolet">Total a pagar - Servicio mensual</span>
         <span>$16.000</span>
       </div>
     </div>
